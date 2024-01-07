7e358606e13f22cfeb2e25f19a1a7566bb3b1202
congpham212 <congpham.bn@gmail.com>
2024-01-07 15:24:58 +0700

    spring boot with hibernate

---
+++ src/test/java/pdcgbbn/learn/spring_boot_with_database/SpringBootWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pdcgbbn.learn.spring_boot_with_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
